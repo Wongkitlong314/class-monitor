4d180ff86228c9d8afdd63a17d017fc3c092b3e1
Yao Chen <yaochenqwer@gmail.com>
2023-10-08 18:42:21 +0800

    filter

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from controllers import user_controller
+from controllers import filter
 
 app = FastAPI()
 
 # 注册路由
 app.include_router(user_controller.router, prefix="/users", tags=["users"])
+app.include_router(filter.router, prefix="/filter", tags=["all"])
+
 
 # 你可以在此处添加其他路由或中间件，或进行其他初始化操作
 
