c73832032bf582db73a79671c27fe722a4774b07
Yao Chen <yaochenqwer@gmail.com>
2023-10-09 02:02:37 +0800

    filter test

--- app/controllers/filter.py
+++ app/controllers/filter.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.services import user_service
-
+from app.models import user
+from app.models import UserMessage
+router = APIRouter()
 router = APIRouter()
 
 
 @router.post("/")
 def filter():
 
-
-    # jianquan
-    return "{}"
+    return UserMessage.text
