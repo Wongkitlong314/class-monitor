d6abe386da2e14c55bac0c3bdc202da983a97f2d
Yao Chen <yaochenqwer@gmail.com>
2023-10-09 01:29:09 +0800

    prompt constructor

--- app/utli/prompt.py
+++ app/utli/prompt.py
@@ -1,15 +1,22 @@
 class PromptConstructor:
-    def __init__(self, template_path: str, args):
+    def __init__(self, template_path: str, *args):
         self.template_path = template_path
         self.args = args
         self.template = ""
-        with open(self.template_path) as f:
+        with open(self.template_path,'r') as f:
             for line in f:
                 self.template += line
                 self.template += "\n"
-
-        self.prompt = ""
-        ## use template and args to generate a prompt
-
+        self.prompt = self.template.format(*args)
     def get(self):
         return self.prompt
+
+
+
+
+if __name__ == "__main__":
+    import os
+    prompt = PromptConstructor("../prompt_templates/prompt_test.txt", "sports","ava","fasfa").get()
+    print(prompt)
+
+
