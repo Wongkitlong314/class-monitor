cac0fafd6a8f370eb7df96e4f7ce033d9410b6d6
Nicolaus Huang <nicolaus.huang@outlook.com>
2023-10-08 10:58:00 +0000

    change to run.sh

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from controllers import user_controller
+from .controllers import user_controller
 
 app = FastAPI()
 
@@ -7,7 +7,3 @@ app = FastAPI()
 app.include_router(user_controller.router, prefix="/users", tags=["users"])
 
 # 你可以在此处添加其他路由或中间件，或进行其他初始化操作
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
