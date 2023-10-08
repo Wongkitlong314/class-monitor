626e1ac20d53cb25c28dfe859bf19d9a0c6de97f
Yao Chen <yaochenqwer@gmail.com>
2023-10-09 05:17:28 +0800

    dao 调同

--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,23 @@
-from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Enum,Sequence, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from app.enums.dao_enum import Role
+from app.enums.dao_enum import Gender
+from app.enums.status_enum import StatusEnum
 
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'student'
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
-    phone_number = Column(String(100), unique=True)
-    token = Column(String(100))
+    phone = Column(String(100), unique=True)
+    role = Column(Enum(Role),unique=True)
+    role_id = Column(Integer,unique=False)
+    gender = Column(Enum(Gender),unique=False)
+    status = Column(Enum(StatusEnum),unique=False)
+
+
+
 
