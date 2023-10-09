26d2b59654c465cb08d3b3c6389ec16ffd054072
Yao Chen <yaochenqwer@gmail.com>
2023-10-09 21:38:43 +0800

    additional enum

--- app/enums/dao_enum.py
+++ app/enums/dao_enum.py
@@ -9,7 +9,12 @@ class Role(Enum):
 class Gender(Enum):
     MALE = "Male"
     FEMALE = "Female"
-
+class grade(Enum):
+    p1_3 = "Primary 1 - 3"
+    p4_6 = "Primary 4 - 6"
+    s1_3 = "Secondary 1- 3"
+    s4_6 = "Secondary 4 - 6"
+    u = "Above"
 
 if __name__ == "__main__":
     print(Role.teacher)
