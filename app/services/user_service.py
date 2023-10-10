47c792dabe85656ef73f4cf6890c00793a498840
Nicolaus Huang <nicolaus.huang@outlook.com>
2023-10-10 22:46:03 +0800

    dashboard

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -18,8 +18,6 @@ def get_all_users():
     result = user_mapper.UserDAO.get_all_users()
     # handle the result here
     return result
-    ...
-
 
 def get_one_user(id=1):
     result = user_mapper.UserDAO.get_one_user(id=id)
