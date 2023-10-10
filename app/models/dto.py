3217480e874aa20176e2782e09d79b177bd2190f
Yao Chen <yaochenqwer@gmail.com>
2023-10-11 02:13:41 +0800

    quiz process

--- app/models/dto.py
+++ app/models/dto.py
@@ -4,12 +4,13 @@ from app.enums.dao_enum import QuestionDifficulty
 
 
 class UserQuizStatus:
-    def __init__(self, size, id=None, reserve_easy=None,
+    def __init__(self, cap, id=None, reserve_easy=None,
                  reserve_medium=None,
                  reserve_hard=None,
                  education_level=None):
         self.id = id
-        self.size = size
+        self.size = 0
+        self.cap = cap
         self.questions = []
         self.answer = []
         self.reserve_easy = reserve_easy
@@ -18,17 +19,25 @@ class UserQuizStatus:
         self.education_level = education_level
         self.fished = False
         self.cur = -1
+        self.score = 0
+        if self.reserve_medium:
+            self.questions.append((self.reserve_medium.pop()))
 
     def get_question(self) -> ButtonResponse:
         self.cur += 1
-        if self.cur > self.size - 1:
+        if self.cur > self.cap - 1:
             return None
 
         question = self.questions[self.cur]
-        resp = ButtonResponse(question.title,
+        resp = ButtonResponse(question.title + "[{}]".format(question.difficulty.value),
                               [button for button in question.choices])
 
         return resp
+
+    def get_cur_question(self):
+        if self.cur < 0:
+            return None
+        return self.questions[self.cur]
 
     def check(self, ans):
         if not self.questions:
@@ -38,6 +47,7 @@ class UserQuizStatus:
         return q.choices.index(ans) == q.answer
 
     def add(self, level: QuestionDifficulty):
+        self.size += 1
         if level == QuestionDifficulty.EASY:
             self.questions.append(self.reserve_easy.pop())
         elif level == QuestionDifficulty.MEDIUM:
--- app/services/quiz_service.py
+++ app/services/quiz_service.py
@@ -23,28 +23,63 @@ def start_quiz(msg: Message):
                               ["Ok", "/exit"])
     elif bot.inner_status == InnerStatus.DOING:
         # doing quiz. iterate the questions.
-        quiz = bot.data.get(data_key, form_quiz(1, 1, 1))
+        quiz = bot.data.get(data_key, form_quiz(3))
+        resps = []
+        pre_ans = msg.text
+        if quiz.cur > -1:
+            cur_question = quiz.get_cur_question()
+            cur_difficulty = cur_question.difficulty
+            if quiz.check(pre_ans):
+                # did correct
+                if cur_difficulty == QuestionDifficulty.HARD:
+                    resps.append(TextResponse("You did right!"))
+                else:
+                    if cur_difficulty == QuestionDifficulty.EASY:
+
+                        quiz.add(QuestionDifficulty.MEDIUM)
+                    else:
+                        quiz.add(QuestionDifficulty.HARD)
+                    resps.append(TextResponse("You did right!" +
+                                              "Let's do more difficult question"))
+                quiz.score += 1
+            else:
+                resps.append(TextResponse("Sorry you did wrongly" +
+                                          "The correct answer" +
+                                          " should be {}".format(cur_question.choices[cur_question.answer]) +
+                                          "\nThe explanation is:\n{}".format(cur_question.explanation)))
+                if not cur_difficulty == QuestionDifficulty.EASY:
+
+                    if cur_difficulty == QuestionDifficulty.HARD:
+                        quiz.add(QuestionDifficulty.MEDIUM)
+                    else:
+                        quiz.add(QuestionDifficulty.EASY)
+                resps.append(TextResponse("Let's do something easier!"))
+
         resp = quiz.get_question()
         if resp:
-            return resp
+            resps.append(resp)
+            return resps
 
     return TextResponse("function developing, but at least you finished the quiz")
 
 
-def form_quiz(easy: int, medium: int, hard: int) -> UserQuizStatus:
+def form_quiz(n) -> UserQuizStatus:
     # randomly form a quiz
     # it is not the most efficient way. will be changed in future
+
     easy_question = QuestionDao.get_by_difficulty(level=QuestionDifficulty.EASY)
     medium_question = QuestionDao.get_by_difficulty(level=QuestionDifficulty.MEDIUM)
     hard_question = QuestionDao.get_by_difficulty(level=QuestionDifficulty.HARD)
 
-    easy_question = random.sample(easy_question, min(easy, len(easy_question)))
-    medium_question = random.sample(medium_question, min(medium, len(medium_question)))
-    hard_question = random.sample(hard_question, min(hard, len(hard_question)))
-    questions = easy_question + medium_question + hard_question
-    random.shuffle(questions)
+    easy_question = random.sample(easy_question, min(n, len(easy_question)))
+    medium_question = random.sample(medium_question, min(n, len(medium_question)))
+    hard_question = random.sample(hard_question, min(n, len(hard_question)))
 
-    return UserQuizStatus(questions=questions)
+    result = UserQuizStatus(n, reserve_easy=easy_question,
+                            reserve_medium=medium_question,
+                            reserve_hard=hard_question)
+
+    return result
 
 
 def quiz_exit(msg: Message):
