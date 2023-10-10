e08c7f3fc8023ec519160dd0d467f7fccae31cfb
kaemp <2919258936@qq.com>
2023-10-10 16:00:32 +0800

    role_play

---
+++ app/core/GPT_stored_message.py
@@ -0,0 +1,20 @@
+import openai
+import time
+openai.api_key = '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'
+
+def get_completion(messages, model="gpt-3.5-turbo", temperature=0):
+    response = ''
+    except_waiting_time = 0.1
+    while response == '':
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                request_timeout=50
+            )
+        except Exception as e:
+            time.sleep(except_waiting_time)
+            if except_waiting_time < 2:
+                except_waiting_time *= 2
+    return response.choices[0].message["content"]
