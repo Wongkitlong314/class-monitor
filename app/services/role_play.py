c2bea8442dde417bed1fd53c2531003f246cacfa
kaemp <2919258936@qq.com>
2023-10-11 04:43:27 +0800

    role_play

--- app/services/role_play.py
+++ app/services/role_play.py
@@ -12,7 +12,6 @@ class Bot:
 # 退出role_play，删掉聊天记录，提示用户role-play结束
 def exit_role_play(user_msg):
     user_bot = session[user_msg.fromNo]
-    print(user_bot)
     del user_bot.data['role_play_message']
     return TextResponse('Role-playing ends. Thank you!')
 
@@ -62,7 +61,6 @@ def start_role_play(user_msg):
         user_bot.data['role_play_msg'] = messages
 
         # 返回chatgpt对话：string
-        print(messages)
         return TextResponse(chat_response)
 
 if __name__ == "__main__":
@@ -74,12 +72,9 @@ if __name__ == "__main__":
     stu = student_mapper.StudentDAO.get_student_by_id(id=stu_id)
     stu_interest = ', '.join(stu.interest)
     stu_edu_level = stu.education_level.value
-    print(stu_interest)
-    print(stu_edu_level)
     speaking_task_description = prompt.PromptConstructor('../prompt_templates/role_play_prompt.txt').get()
     messages = [
         {"role": "system", "content": speaking_task_description},
         {"role": "user", "content": "My Education level is: %s " % (stu_edu_level)},
         {"role": "user", "content": "My interest is: %s " % (stu_interest)}]
     chat_response = get_completion(messages, temperature=0.7)
-    print(chat_response)
