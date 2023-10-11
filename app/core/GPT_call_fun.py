ba01b2d075dc47e3941e101cb6da48f61d45541b
kaemp <2919258936@qq.com>
2023-10-11 12:35:32 +0800

    writing

--- app/core/GPT_call_fun.py
+++ app/core/GPT_call_fun.py
@@ -2,9 +2,41 @@ import openai
 import time
 openai.api_key = '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'
 
-def get_completion(messages, model="gpt-3.5-turbo-0613", fun=None,temperature=0):
+def get_completion(messages, model="gpt-3.5-turbo-0613",temperature=0):
     response = ''
     except_waiting_time = 0.1
+    fun = [
+    {
+        "name": "grading_student_writing",
+        "description": """You are an English teacher. 
+        Generate fair feedback and reviews for student writing.
+            Rate the writing in these aspects:
+            - Content and Relevance
+            - Organization and Structure
+            - Grammar and Mechanics
+            - Vocabulary and Word Choice
+            - Clarity and Cohesion
+
+            For each aspect, score it on a scale of 0 to 5. 
+            The format should be "score/5". 
+            Provide precise and concise explanations for your ratings. 
+            Students may ask you to explain your ratings and comments.
+            """,
+
+        "parameters": {
+            "type": "object",
+            "description": "Comment and score for student writing.",
+            "properties": {
+                "comment": {
+                    "type": "string",
+                    "description": "Comment and score for student writing. Should have around 200 words"
+
+                }
+            }
+        },
+        "required": ["comment"]
+    }
+]
     while response == '':
         try:
             response = openai.ChatCompletion.create(
