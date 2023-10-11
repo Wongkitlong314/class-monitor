99f5adb91fd29aa0d761afeca6a2fd7d9787d4f0
Yao Chen <yaochenqwer@gmail.com>
2023-10-11 10:05:50 +0800

    dash board

--- app/config/config.py
+++ app/config/config.py
@@ -116,3 +116,141 @@ FUNCTIONS_WITH_INTRO = FUNCTIONS + [{
     "description": "This function will introduce all functions of the system"
 
 }]
+CHARTS = [
+        {
+            "id":0,
+            "name": "Bar_Chart",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "labels": {
+                        "type": "string",
+                        "description": "A label for the dataset which appears in the legend and tooltips, mostly time related",
+                    },
+                    "datasets": {
+                        "type": "array",
+                        "type": "string",
+                        "description": "An array of numbers representing the data values for the corresponding labels.",
+                    },
+                },
+                "required": ["labels", "datasets"],
+            },
+            "description":"A bar chart provides a visual representation of categorical data with rectangular bars where the lengths are proportional to the values they represent. It can be used to compare different categories of data."
+        },
+        {
+            "id": 1,
+            "name": "Line_Chart",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "labels": {
+                        "type": "string",
+                        "description": "A label for the dataset which appears in the legend and tooltips, typically time or categorical data points.",
+                    },
+                    "datasets": {
+                        "type": "array",
+                        "type": "string",
+                        "description": "An array of numbers representing the data values plotted against the corresponding labels.",
+                    }
+                },
+                "required": ["labels", "datasets"],
+            },
+            "description": "A line chart displays information as a series of data points connected by straight line segments. It's often used to visualize a trend in data over intervals of time."
+        },
+        {
+            "id": 2,
+            "name": "Pie_Chart",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "labels": {
+                        "type": "string",
+                        "description": "A label for the dataset which appears in the legend and tooltips, representing distinct categories.",
+                    },
+                    "datasets": {
+                        "type": "array",
+                        "type": "string",
+                        "description": "An array of numbers indicating the proportion of each segment in the pie."
+                    }
+                },
+                "required": ["labels", "datasets"],
+            },
+            "description": "A pie chart is a circular statistical graphic divided into slices to illustrate numerical proportion. Each slice corresponds to a category and its size is proportional to its value."
+        },
+        {
+            "id": 3,
+            "name": "Radar_Chart",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "labels": {
+                        "type": "string",
+                        "description": "A label for the dataset which appears in the legend and tooltips, representing different metrics or criteria.",
+                    },
+                    "datasets": {
+                        "type": "string",
+                        "description": "An array of numbers showing the performance or value for each criterion on the radar."
+                    }
+                },
+                "required": ["labels", "datasets"],
+            },
+            "description": "A radar chart is a graphical method of displaying multivariate data in the form of a two-dimensional chart, representing three or more quantitative variables on axes starting from the same point."
+        },
+        {
+            "id": 4,
+            "name": "Scatter_Chart",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "labels": {
+                        "type": "string",
+                        "description": "A label for the dataset which appears in the legend and tooltips, typically representing individual data points.",
+                    },
+                    "datasets": {
+                        "type": "string",
+                        "description": "An array of numbers which are plotted to represent the relationship between two variables."
+                    }
+                },
+                "required": ["labels", "datasets"],
+            },
+            "description": "A scatter chart uses dots to represent values for two different variables. The position of each dot represents its value for those two variables."
+        }
+
+]
+
+CHARTS_CONFIGS = [
+    {
+        "type": "bar",
+        "data": {
+            "labels": [],
+            "datasets": []
+        }
+    },
+    {
+        "type": "line",
+        "data": {
+            "labels": [],
+            "datasets": []
+        }
+    },
+    {
+        "type": "pie",
+        "data": {
+            "labels": [],
+            "datasets": []
+        }
+    },
+    {
+        "type": "radar",
+        "data": {
+            "labels": [],
+            "datasets": []
+        }
+    },
+    {
+        "type": "scatter",
+        "data": {
+            "datasets": []
+        }
+    }
+]
